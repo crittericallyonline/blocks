36493574e0e84c534fa78e17903bf7a3e3bb3bc5
crittericallyonline <ballstenny@gmail.com>
2025-12-15 14:42:47 -0800

    textures

--- src/textures.h
+++ src/textures.h
@@ -5,33 +5,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-// some of it is from: https://open.gl/textures
-GLuint generate_checker()
-{
-    GLuint image;
-    // glActiveTexture(GL_TEXTURE0); // idk what this does so lets disable it and see what happens
-
-    glGenTextures(1, &image);
-    glBindTexture(GL_TEXTURE_2D, image);
-    // Black/white checkerboard
-    float pixels[] = {
-        0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 1.0f,
-        1.0f, 0.0f, 1.0f,   0.0f, 0.0f, 0.0f
-    };
-
-    // now for binding data to the buffer
-    // I HATE SAMPLING, pixel art ftw
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    return image;
-}
-
 GLuint load_image(const char *imagePath)
 {
     GLuint image;
@@ -65,16 +38,15 @@ GLuint load_image(const char *imagePath)
 
 
 struct Textures {
-    GLuint index[5];
+    GLuint index[4];
 } Textures;
 
 void init_textures()
 {
     Textures.index[0] = load_image("/tex/stone.png");
-    // Textures.index[1] = load_image("/tex/dirt.png");
-    // Textures.index[2] = load_image("/tex/grass.png");
+    Textures.index[1] = load_image("/tex/dirt.png");
+    Textures.index[2] = load_image("/tex/grass.png");
     Textures.index[3] = load_image("/tex/oak_planks.png");
-    Textures.index[4] = generate_checker();
 }
 
 #endif//PROGRAM_H
